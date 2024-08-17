01d7652f5dc303799e2a29b6f786ef3409ae1f92
Nyanneko0113 <yuuki01130201@gmail.com>
2024-08-18 00:00:13 +0900

    jsonによる設定ファイルを追加

--- src/main/java/org/nyanneko0113/discord_bot/Main.java
+++ src/main/java/org/nyanneko0113/discord_bot/Main.java
@@ -9,20 +9,28 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import org.nyanneko0113.discord_bot.commands.ReservedCommand;
+import org.nyanneko0113.discord_bot.manager.ConfigManager;
 
 import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 
 public class Main extends ListenerAdapter implements EventListener {
 
     private static JDA jda;
-    private static final String token = "";
 
     public static void main(String[] strings) throws LoginException, InterruptedException, URISyntaxException, IOException {
+        if (ConfigManager.getToken().equalsIgnoreCase("") || ConfigManager.getToken() == null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("tokenを設定してください：");
+            ConfigManager.setToken(reader.readLine());
+        }
+
         if (jda == null) {
-            jda = JDABuilder.createDefault(token)
+            jda = JDABuilder.createDefault(ConfigManager.getToken())
                     .addEventListeners(new Main())
                     .addEventListeners(new ReservedCommand())
                     .build();
---
+++ src/main/java/org/nyanneko0113/discord_bot/manager/ConfigManager.java
@@ -0,0 +1,68 @@
+package org.nyanneko0113.discord_bot.manager;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.nyanneko0113.discord_bot.Main;
+
+import java.io.*;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ConfigManager {
+
+    public static String getToken() throws IOException {
+         return getJson().get("token").getAsString();
+    }
+
+    public static void setToken(String token) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(getFile()))) {
+
+            JsonObject json = new Gson().fromJson(reader, JsonObject.class);
+            json.addProperty("token", token);
+
+            try (BufferedWriter write = new BufferedWriter(new FileWriter(getFile()))) {
+                write.write(json.toString());
+            }
+        }
+    }
+
+    private static JsonObject getJson() throws IOException {
+        if (getFile().exists()) {
+            StringBuilder file_read = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(Files.newInputStream(getFile().toPath()), StandardCharsets.UTF_8))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    file_read.append(line);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return new JsonParser().parse(file_read.toString()).getAsJsonObject();
+        }
+        else {
+            throw new NullPointerException("ファイルが存在しません");
+        }
+    }
+
+    private static File getFile() throws IOException {
+        File file = new File("bot_config.json");
+
+        if (!file.exists()) {
+            file.createNewFile();
+
+            try (BufferedWriter write = new BufferedWriter(new FileWriter(file))) {
+                JsonObject json = new JsonObject();
+                json.addProperty("token", "");
+                write.write(json.toString());
+            }
+        }
+        return file;
+    }
+
+}
